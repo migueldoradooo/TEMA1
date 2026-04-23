bcba491458a861ad2f359e35bf20b16038899a3b
migueldoradooo <migueldoradomoreno57@gmail.com>
2026-04-23 11:35:41 +0200

    JavaScript CodecCata

---
+++ javascript/miSegundoJavascript.js
@@ -0,0 +1,40 @@
+let myName = 'Marisol';
+let edad = 7;
+const pi = 3.1416;
+
+console.log(myName);
+console.log(typeof(myName));
+console.log(typeof(edad));
+
+let stredad = String(edad); // He realizado la operacion de Casting
+
+console.log('he cambiado el tipo de variable');
+console.log(stredad);
+console.log(typeof(stredad));
+
+
+
+function saludar(){
+        console.log('Hola soy Miguel Ángel 2026');
+        //document.write('Hola soy Miguel Ángel 2026');
+        // alert('Hola soy Miguel Ángel 2026');
+};
+
+//declaramos las variables num1 y num2 
+let num1 = 1
+let num2 = 2
+//creamos una funcion que los sume
+function Sumar(a,b){ //a y b son parametros
+        return a + b; //el retuen sirve para que la funcion obtenga un valor, en este caso, el se la suma
+        
+}
+
+
+
+
+
+saludar();
+document.write('<br> la suma total es: '+Sumar(num1,num2)); //1 y 2 son argumentos
+
+/**/
+ 

2696e709ff8c2101f56938d154f90cc6ac42fb1f
migueldoradooo <migueldoradomoreno57@gmail.com>
2026-04-24 08:39:31 +0200

    JavaScript

--- javascript/codecata.js
+++ javascript/codecata.js
@@ -1,3 +1,20 @@
-function getCopyright(name, year){
-    
+
+//Callback
+
+let copyright = function getCopyright(name, year, formatter){
+                    let copyright = formatter(name,year);
+                    return copyright
 }
+
+let formatter = function formatter(name,year){
+    return name + " | " + year;
+}
+
+
+console.log(resultado("Miguel Angel", 2026,formatter));
+
+// Autoinvocar una funcion
+
+(function(name, year){
+    console.log(name,year);
+})("Miguel Angel, 2027");
--- javascript/miSegundoJavascript.js
+++ javascript/miSegundoJavascript.js
@@ -20,13 +20,9 @@ function saludar(){
         // alert('Hola soy Miguel Ángel 2026');
 };
 
-//declaramos las variables num1 y num2 
-let num1 = 1
-let num2 = 2
 //creamos una funcion que los sume
-function Sumar(a,b){ //a y b son parametros
+function sumar(a,b){ //a y b son parametros
         return a + b; //el retuen sirve para que la funcion obtenga un valor, en este caso, el se la suma
-        
 }
 
 
@@ -34,7 +30,22 @@ function Sumar(a,b){ //a y b son parametros
 
 
 saludar();
-document.write('<br> la suma total es: '+Sumar(num1,num2)); //1 y 2 son argumentos
-
+//document.write('<br> la suma total es: /n '+sumar(num1,num2));
+//document.write(`<br> la suma total es: /n ${sumar(num1,num2)}`);
 /**/
- 
+
+//declaramos las variables num1 y num2 
+let num1 = 1
+let num2 = 2
+suma = sumar(num1,num2)
+console.log(`La suma ${num1} y \n${num2} es:${suma}`); // \n solo en console.log y en alert
+
+
+//FUNCIONES AutoInvocadas
+
+//AutoInvocar funciones
+
+(function(num1,num2 = 2,num3)
+{
+        console.log(num1 + num2+num3)
+}(4,undefined,10));
